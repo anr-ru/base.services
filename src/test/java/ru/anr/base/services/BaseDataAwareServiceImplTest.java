38894e3def4b3a4f29760232965747f920c2459f
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2014-12-02 17:22:29 +0500

    Trying this variant. For some reason doesn't compile on CI.

--- src/test/java/ru/anr/base/services/BaseDataAwareServiceImplTest.java
+++ src/test/java/ru/anr/base/services/BaseDataAwareServiceImplTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-import ru.anr.base.samples.dao.BaseDao;
+import ru.anr.base.dao.repository.BaseRepository;
 import ru.anr.base.samples.domain.Samples;
 
 /**
@@ -43,7 +43,7 @@ public class BaseDataAwareServiceImplTest extends BaseServiceTestCase {
         s = impl.dao().save(s);
         Assert.assertNotNull(s.getId());
 
-        BaseDao d = impl.dao();
+        BaseRepository<Samples> d = impl.dao();
 
         Samples e = d.findOne(s.getId());
         Assert.assertEquals(s, e);
