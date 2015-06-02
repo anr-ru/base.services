71f19bcbbeeb4d7434117c40ff04eac720a6edb6
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2015-06-02 11:04:38 +0500

    Added a new additional functino to create safe paging parameters

--- src/main/java/ru/anr/base/services/api/AbstractApiCommandStrategyImpl.java
+++ src/main/java/ru/anr/base/services/api/AbstractApiCommandStrategyImpl.java
@@ -15,14 +15,18 @@
  */
 package ru.anr.base.services.api;
 
+import java.util.Optional;
+
 import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 
 import ru.anr.base.domain.api.APICommand;
 import ru.anr.base.services.BaseServiceImpl;
 
 /**
- * Implementation of base Api Command strategy withot using {@link ApiMethod}
- * annotation.
+ * Implementation of base API Command strategy.
  *
  * @author Alexey Romanchuk
  * @created Nov 10, 2014
@@ -83,5 +87,25 @@ public class AbstractApiCommandStrategyImpl extends BaseServiceImpl implements A
     public Object delete(APICommand cmd) {
 
         throw new NotImplementedException("Method 'Delete' not implemented");
+    }
+
+    /**
+     * Prepares a {@link Pageable} object based on provided data. Handles the
+     * situation when the paging parameters are nulls.
+     * 
+     * @param cmd
+     *            An original API Command
+     * @param direction
+     *            A direction of sorting
+     * @param properties
+     *            Names of properties used for sorting
+     * @return A new created {@link Pageable}
+     */
+    protected Pageable safePageable(APICommand cmd, Direction direction, String... properties) {
+
+        Integer page = Optional.ofNullable(cmd.getRequest().getPage()).orElse(0);
+        Integer perPage = Optional.ofNullable(cmd.getRequest().getPerPage()).orElse(0);
+
+        return new PageRequest(page, perPage, direction, properties);
     }
 }
