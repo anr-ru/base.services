1d215fe1d2d3103d60ee22ab1c2f871853bbbce4
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2015-06-06 10:01:25 +0000

    In some case we need just a very simple set of states

---
+++ src/main/java/ru/anr/base/domain/BasicStates.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ru.anr.base.domain;
+
+/**
+ * Basic states which can be used in many cases.
+ *
+ *
+ * @author Alexey Romanchuk
+ * @created Jun 5, 2015
+ *
+ */
+
+public enum BasicStates {
+
+    /**
+     * A new object
+     */
+    New,
+    /**
+     * Enabled object
+     */
+    Active,
+    /**
+     * Disabled object
+     */
+    Inactive;
+}
