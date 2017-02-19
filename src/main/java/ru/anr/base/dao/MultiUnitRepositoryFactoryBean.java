955ee8a1a7ea1a77503c3e9ea0f51e9f43f63322
ebalukova <ebalukova@gmail.com>
2017-02-19 23:58:12 +0500

    while not working revert

--- src/main/java/ru/anr/base/dao/MultiUnitRepositoryFactoryBean.java
+++ src/main/java/ru/anr/base/dao/MultiUnitRepositoryFactoryBean.java
@@ -43,17 +43,6 @@ public class MultiUnitRepositoryFactoryBean<T extends BaseEntity> extends
         TransactionalRepositoryFactoryBeanSupport<BaseRepository<T>, T, Long> {
 
     /**
-     * Creates a new {@link MultiUnitRepositoryFactoryBean} for the given repository interface.
-     *
-     * @param repositoryInterface
-     *            must not be {@literal null}.
-     */
-    public MultiUnitRepositoryFactoryBean(Class<? extends BaseRepository<T>> repositoryInterface) {
-
-        super(repositoryInterface);
-    }
-
-    /**
      * A reference to the current {@link EntityManager}
      */
     private EntityManager entityManager;
