1e2be6e441dc3a85e4b93e349b01ea01c6d2b1b0
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2023-12-12 16:56:55 +0500

    Make the validators loading synchronized.
    (cherry picked from commit ca33cde2833b2570f541e001cf1226525ff08936)

--- src/test/java/ru/anr/base/services/validation/ValidationTest.java
+++ src/test/java/ru/anr/base/services/validation/ValidationTest.java
@@ -7,6 +7,10 @@ import ru.anr.base.ApplicationException;
 import ru.anr.base.samples.domain.Samples;
 import ru.anr.base.samples.services.validation.SampleService;
 import ru.anr.base.services.BaseLocalServiceTestCase;
+import ru.anr.base.tests.multithreading.ThreadExecutor;
+import ru.anr.base.tests.multithreading.ThreadJob;
+
+import java.util.LinkedHashMap;
 
 /**
  * Basic validation tests.
@@ -38,5 +42,25 @@ public class ValidationTest extends BaseLocalServiceTestCase {
         } catch (ApplicationException ex) {
             Assertions.assertEquals("Error", ex.getMessage());
         }
+    }
+
+    @Autowired
+    private ValidationFactory factory;
+
+    /**
+     * Use case: check multi-threading access to validators
+     */
+    @Test
+    public void testMultiThreadAccessToValidators() {
+
+        ThreadExecutor exec = new ThreadExecutor();
+        for (int i = 1; i <= 20; i++) {
+            exec.add(new ThreadJob(x -> {
+                logger.debug("Found {} validators", factory.getValidators(Samples.class).size());
+            }));
+        }
+
+        exec.start();
+        Assertions.assertTrue(exec.waitNotError());
     }
 }
